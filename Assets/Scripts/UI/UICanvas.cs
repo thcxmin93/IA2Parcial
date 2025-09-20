afaf745abe3549e985754b456a349773c8a82eb0
thcxmin93 <martu.ieraci@hotmail.com>
2025-09-20 14:00:10 -0300

    TMPRO

---
+++ Assets/Scripts/UI/UICanvas.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICanvas : MonoBehaviour
+{
+    public TurretController turret;
+    [SerializeField] private GameObject _textEnemies;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
